d540ddc6a3fbea286d436a7e2db305bd7d04e663
Hui <muemily2@gmail.com>
2025-12-29 13:51:31 +0000

    fix/comment format

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import ToDoList from "./ToDoList.jsx";
 export default function App() {
   return (
     <StrictMode>
-      <div className="App">  //TODO: 这个文件又用了strictMode，有必要吗？root已经用stritmode包裹了。
+      <div className="App">  
+        {/* TODO: 这个文件又用了strictMode，有必要吗？root已经用stritmode包裹了。 */}
         <ToDoList />
       </div>
     </StrictMode>
